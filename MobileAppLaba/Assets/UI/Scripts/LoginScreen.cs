5c97b73e9d6a51e9a54654011d296592bc39ff53
filippov_filipp <imawakebrba@gmail.com>
2023-03-28 04:21:24 +0700

    native plugin and login screen

--- MobileAppLaba/Assets/UI/Scripts/LoginScreen.cs
+++ MobileAppLaba/Assets/UI/Scripts/LoginScreen.cs
@@ -1,8 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
+using FantomLib;
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginScreen : UIWindow
 {
-    
+    [SerializeField] private TMP_InputField _name;
+    [SerializeField] private TMP_InputField _email;
+    [SerializeField] private TMP_InputField _password;
+    [SerializeField] private Button _showPasswordButton;
+
+    [SerializeField] private Image _showPasswordButtonImage;
+    [SerializeField] private Sprite _showPasswordSprite;
+    [SerializeField] private Sprite _hidePasswordSprite;
+
+    [SerializeField] private Button _signUpButton;
+    [SerializeField] private Button _logInButton;
+
+    [SerializeField] private OKDialogController _oKDialogController;
+
+    private bool _showPassword;    
+
+    private bool ShowPassword
+    {
+        get => _showPassword;
+        set
+        {
+            if(value)
+            {
+                _password.contentType = TMP_InputField.ContentType.Standard;
+                _showPasswordButtonImage.sprite = _showPasswordSprite;
+            }
+            else
+            {
+                _password.contentType = TMP_InputField.ContentType.Password;
+                _showPasswordButtonImage.sprite = _hidePasswordSprite;
+            }
+            _showPassword = value;
+        }
+    }
+
+    public event EventHandler SignedUp;    
+
+    private void OnEnable()
+    {
+        _showPasswordButton.onClick.AddListener(OnShowPasswordButtonClick);
+        _signUpButton.onClick.AddListener(OnSignUpButtonClick);
+        _logInButton.onClick.AddListener(OnSignUpButtonClick);
+    }    
+
+    private void OnSignUpButtonClick()
+    {
+        if(!string.IsNullOrEmpty(_name.text) && !string.IsNullOrEmpty(_email.text) && !string.IsNullOrEmpty(_password.text))
+        {
+            SignedUp?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            //dialogue
+            _oKDialogController.Show();
+        }
+    }
+
+    private void OnShowPasswordButtonClick()
+    {
+        ShowPassword = !_showPassword;
+    }
+
+    private void OnDisable()
+    {
+        _showPasswordButton.onClick.RemoveListener(OnShowPasswordButtonClick);
+        _signUpButton.onClick.RemoveListener(OnSignUpButtonClick);
+        _logInButton.onClick.RemoveListener(OnSignUpButtonClick);
+    }
 }
