93821d9925caae2719d5658d58f6afe36eaa31b2
filippov_filipp <imawakebrba@gmail.com>
2023-03-31 00:58:44 +0700

    search logic

--- MobileAppLaba/Assets/UI/Scripts/Dictionary/DictionaryItemConfig.cs
+++ MobileAppLaba/Assets/UI/Scripts/Dictionary/DictionaryItemConfig.cs
@@ -1,18 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class DictionaryItemConfig : MonoBehaviour
+[CreateAssetMenu(fileName = "DictionaryItemConfig", menuName = "ScriptableObjects/DictionaryItemConfig", order = 1)]
+public class DictionaryItemConfig : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private DictionaryItemModel _placeholderModel;
+
+    [SerializeField] private DictionaryItemModel[] _models;
+
+    [NonSerialized] private bool _inited;
+
+    private Dictionary<string, DictionaryItemModel> _dict;
+
+    public DictionaryItemModel Placeholder => _placeholderModel;
+
+    public DictionaryItemModel Get(string title)
     {
-        
+        if(!_inited)
+        {
+            Init();
+        }
+        _dict.TryGetValue(title, out var model);
+        return model;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Init()
     {
-        
+        _dict = new Dictionary<string, DictionaryItemModel>();
+
+        foreach (var item in _models)
+        {
+            _dict.TryAdd(item.Title, item);
+        }
+
+        _inited = true;
     }
 }
--- MobileAppLaba/Assets/UI/Scripts/Dictionary/DictionaryWindow.cs
+++ MobileAppLaba/Assets/UI/Scripts/Dictionary/DictionaryWindow.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,4 +10,33 @@ public class DictionaryWindow : BaseContentWindow
     [SerializeField] private TMP_Text _partOfSpeechText;
     [SerializeField] private DictionaryTextBox _firstTextBox;
     [SerializeField] private DictionaryTextBox _secondTextBox;
+    [SerializeField] private DictionaryItemConfig _config;
+
+    private void OnEnable()
+    {
+        _searchButton.onClick.AddListener(OnSearchButtonClick);
+    }
+
+    private void OnSearchButtonClick()
+    {
+        var model = _config.Get(_searchField.text);
+        if(model == null)
+        {
+            model = _config.Placeholder;
+        }
+        SetModel(model);
+    }
+
+    private void SetModel(DictionaryItemModel model)
+    {
+        _title.text = model.Title;
+        _partOfSpeechText.text = model.PartOfSpeech;
+        _firstTextBox.SetContent(model.Meanings[0].FirstText, model.Meanings[0].SecondText);
+        _secondTextBox.SetContent(model.Meanings[1].FirstText, model.Meanings[1].SecondText);
+    }
+
+    private void OnDisable()
+    {
+        _searchButton.onClick.RemoveListener(OnSearchButtonClick);
+    }
 }
